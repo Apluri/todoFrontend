87b3bb36a2383fee38b6a1ab1d53294d55957a25
salmi <toni.salminen89@gmail.com>
2020-11-18 16:56:25 +0200

    enable listing of deadlines on tasks

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,11 +1,14 @@
 import React from "react";
 const TodoList = ({ todos, handleDelete }) => {
+  console.log(todos);
+
   return (
     <div>
       <h1 className="todo-items">Things to do:</h1>
       {todos.map((todo) => (
         <div className="todo-item" key={todo.id}>
           <div> {todo.task} </div>
+          <div> {todo.deadline} </div>
           <div>
             <button onClick={() => handleDelete(todo.id)}>Delete</button>
           </div>

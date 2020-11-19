c1f0312013cf496d497953a8a84f871b33b57f9b
salmi <toni.salminen89@gmail.com>
2020-11-19 18:32:33 +0200

    restructure divs so that BurgerButton is always visible even if navbar is toggled off

--- src/components/LeftNav.js
+++ src/components/LeftNav.js
@@ -1,37 +1,44 @@
 // useState to replace need for props usage
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom"; // import { Link } from "react-router-dom";
 
 const LeftNav = ({ navSize, handleNavSizeChange }) => {
+  //test
+  let blurring;
+  //for enabling dynamic blurring on mobile screen only
+  navSize === "100%" ? (blurring = true) : (blurring = false);
+  console.log(blurring);
+  //<div className={navSize === "100%" ? "one" : "two"}>
   return (
-    <div className="left-sidebar" style={{ width: navSize }}>
-      <button
-        className="burger-btn"
-        onClick={() => {
-          handleNavSizeChange();
-        }}
-      >
+    <div>
+      <div className="left-sidebar" style={{ width: navSize }}>
+        <nav className="navbar">
+          <NavLink
+            to={"/home"}
+            className="navlink-home"
+            activeClassName={"active"}
+          >
+            Home
+          </NavLink>
+          <NavLink
+            to={"/folders"}
+            className="navlink"
+            activeClassName={"active"}
+          >
+            Folders
+          </NavLink>
+          <NavLink
+            to={"/settings"}
+            className="navlink"
+            activeClassName={"active"}
+          >
+            Settings
+          </NavLink>
+        </nav>
+      </div>
+      <button className="burger-btn" onClick={() => handleNavSizeChange()}>
         Burg
       </button>
-      <nav className="navbar">
-        <NavLink
-          to={"/home"}
-          className="navlink-home"
-          activeClassName={"active"}
-        >
-          Home
-        </NavLink>
-        <NavLink to={"/folders"} className="navlink" activeClassName={"active"}>
-          Folders
-        </NavLink>
-        <NavLink
-          to={"/settings"}
-          className="navlink"
-          activeClassName={"active"}
-        >
-          Settings
-        </NavLink>
-      </nav>
     </div>
   );
 };

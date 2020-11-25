d2aae283f14e84cf2e3c8547c5a5c6bcf84805dd
salmi <toni.salminen89@gmail.com>
2020-11-25 20:24:34 +0200

    non null deadline formatting toDateString

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,13 +2,19 @@ import React from "react";
 const TodoList = ({ todos, handleDelete }) => {
   console.log(todos);
 
+  let sqlDateToDateString = (d) => {
+    let temp;
+    d == null ? (temp = "") : (temp = new Date(d).toDateString());
+    return temp;
+  };
+
   return (
     <div>
       <h1 className="todo-items">Things to do:</h1>
       {todos.map((todo) => (
         <div className="todo-item" key={todo.id}>
           <div> {todo.task} </div>
-          <div> {todo.deadline} </div>
+          <div> {sqlDateToDateString(todo.deadline)} </div>
           <div>
             <button onClick={() => handleDelete(todo.id)}>Delete</button>
           </div>

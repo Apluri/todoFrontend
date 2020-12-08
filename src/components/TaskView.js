a3d3a3832ceebb2a4db838ff73f4f47e8b06844d
salmi <toni.salminen89@gmail.com>
2020-12-08 23:50:25 +0200

    null-check

--- src/components/TaskView.js
+++ src/components/TaskView.js
@@ -3,7 +3,28 @@ import React from "react";
 const TaskView = ({ folders, selectedTask }) => {
   console.log(selectedTask);
   console.log(folders);
-  return <>Selected task: {selectedTask.title}</>;
+  const checkIfNull = () => {
+    return selectedTask === null ? "" : selectedTask;
+  };
+  return (
+    <>
+      <h2>{checkIfNull().title}</h2>
+      <form>
+        <div className="description-box">
+          <textarea type="text" value={checkIfNull().description} />
+        </div>
+      </form>
+      <button>save</button>
+      <br />
+      <>
+        Task deadline:{" "}
+        {checkIfNull().deadline === null
+          ? "No date selected"
+          : checkIfNull().deadline}
+      </>
+      <button>edit</button>
+    </>
+  );
 };
 
 export default TaskView;

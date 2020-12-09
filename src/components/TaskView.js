a4e8f6c324e5fe036d7f50ecd74fd46aa6a150f1
salmi <toni.salminen89@gmail.com>
2020-12-09 19:30:03 +0200

    use components on return

--- src/components/TaskView.js
+++ src/components/TaskView.js
@@ -1,54 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
+import TaskViewEdit from "./TaskViewEdit";
+import TaskViewShow from "./TaskViewShow";
 
 const TaskView = ({ folders, selectedTask }) => {
   const history = useHistory();
   const redirect = () => {
     history.push("/");
   };
-
-  /* :DD
-  const checkIfNull = (argument) => {
-    return argument === null ? true : false;
-  };
-  */
-  let sqlDateToDateString = (d) => {
-    if (d !== null) {
-      let temp = new Date(d);
-      let dateFormat = `${("0" + temp.getDate()).slice(-2)} - ${(
-        "0" +
-        (temp.getMonth() + 1)
-      ).slice(-2)}`;
-      return dateFormat;
-    } else {
-      return "";
-    }
-  };
+  const [editMode, setEditMode] = useState(false);
 
   return !selectedTask ? (
     <> {redirect()}</>
+  ) : editMode ? (
+    <>
+      <TaskViewEdit folders={folders} selectedTask={selectedTask} />
+      <button onClick={() => setEditMode(!editMode)}>save</button>
+    </>
   ) : (
     <>
-      <h2>{selectedTask.title}</h2>
-      <form>
-        <div className="description-box">
-          <textarea
-            type="text"
-            placeholder="Task description (optional)"
-            value={selectedTask.description ? selectedTask.description : ""}
-            onChange={(e) => console.log(e)}
-          />
-        </div>
-      </form>
-      <button>save</button>
-      <br />
-      <div>
-        Task deadline:
-        {!selectedTask.deadline
-          ? "No date selected"
-          : sqlDateToDateString(selectedTask.deadline)}
-      </div>
-      <button>edit</button>
+      <TaskViewShow folders={folders} selectedTask={selectedTask} />
+      <button onClick={() => setEditMode(!editMode)}>edit</button>
     </>
   );
 };

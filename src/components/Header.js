36f4ea9dd27c29f477f42075ce50f3aa8b7010ef
Apluri <aleksishot2@gmail.com>
2020-12-11 19:46:03 +0200

    Add search feature, search overlaps with other stylings such as sort meaning one of them can be used at the time"

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,32 @@
 // useState to replace need for props usage
-import React from "react";
+import React, { useState } from "react";
 import TitleComponent from "./TitleComponent";
 
-const Header = () => {
+const Header = ({ searchData }) => {
+  const [currInput, setCurrInput] = useState("");
+  const handleSubmit = (e) => {
+    // prevent reload?
+    e.preventDefault();
+    if (currInput.length > 0) {
+      searchData(currInput);
+      setCurrInput("");
+    } else {
+      // null means fetchAll
+      searchData(null);
+    }
+  };
   return (
     <div className="header">
       <TitleComponent />
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <input
+          type="text"
+          placeholder="title, description, deadline"
+          value={currInput}
+          onChange={(e) => setCurrInput(e.target.value)}
+        />
+        <button>Search</button>
+      </form>
     </div>
   );
 };
